845d977e1b3f0a25d657889f4312c4b830a6f5c5
harikiran713 <harikiranl713@gmail.com>
2024-10-15 22:27:17 +0530

    this is example for login with  firbas

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+import './App.css';
+import { app } from './firebase';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formdata, setformdata] = useState({});
+
+  const collect = (event) => {
+    setformdata({
+      ...formdata,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const auth = getAuth(app);
+
+  const submit = (e) => {
+    e.preventDefault();
+    const { email, password } = formdata;
+    signInWithEmailAndPassword(auth, email, password)
+      .then((value) => {
+        console.log(value);
+      })
+      .catch((error) => {
+        console.error('Error signing in:', error);
+      });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <form onSubmit={submit}>
+        <label htmlFor="email">Email</label>
+        <input type="text" id="email" name="email" onChange={collect} />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" onChange={collect} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
